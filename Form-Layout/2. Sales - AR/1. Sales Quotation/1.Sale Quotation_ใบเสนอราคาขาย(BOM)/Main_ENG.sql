9e744c95a34b6657e1d0f87d90102704eb6fdc95
NatthasitC-Arty <Natthasit@sala-daeng.com>
2026-04-19 01:53:08 +0700

    Done SQ FC

---
+++ Form-Layout/2. Sales - AR/1. Sales Quotation/1.Sale Quotation_ใบเสนอราคาขาย(BOM)/Main_ENG.sql
@@ -0,0 +1,93 @@
+﻿SELECT DISTINCT
+case when OCRD.Phone2 is null then ''
+  when OCRD.Phone2 is not null then ', ' + OCRD.Phone2
+  END 'Phone2',
+CONCAT(OCPR.FirstName,' ',OCPR.LastName) AS 'Coontact',
+OQUT.DocEntry,
+OQUT.[Address],
+OCRD.U_SLD_Title,
+OCRD.U_SLD_FullName,
+CRD1.GlblLocNum,
+OCRD.Phone1,
+ISNULL(OCRD.Phone2,'') As 'Phone2',
+OCRD.Fax,
+OCRD.LicTradNum,
+NNM1.BeginStr,
+OQUT.DocNum,
+OQUT.DocDate,
+OQUT.DocDueDate,
+(QUT1.VisOrder) As 'No.',
+QUT1.LineNum as 'Line No.', 
+QUT1.ItemCode,
+OITM.FrgnName AS 'Dscription',
+QUT1.Quantity,
+QUT1.PriceBefDi,
+CASE
+	WHEN OQUT.DocCur = 'THB' THEN QUT1.LineTotal
+	ELSE QUT1.TotalFrgn
+END AS 'LineTotal',
+CASE
+	WHEN OQUT.DocCur = 'THB' THEN OQUT.DiscSum
+	ELSE OQUT.DiscSumFC
+END AS 'DiscSum',
+CASE
+	WHEN OQUT.DocCur = 'THB' THEN OQUT.VatSum
+	ELSE OQUT.VatSumFC
+END AS 'VatSum',
+CASE
+	WHEN OQUT.DocCur = 'THB' THEN OQUT.DocTotal
+	ELSE OQUT.DocTotalFC
+END AS 'DocTotal',
+SUM(
+	CASE
+		WHEN OQUT.DocCur = 'THB' THEN QUT1.LineTotal
+		ELSE QUT1.TotalFrgn
+	END
+) OVER() AS 'Sum_LineTotal_All',
+OQUT.DiscPrcnt As 'DiscP',
+OQUT.DocCur ,
+OCPR.FirstName,
+OCPR.LastName,
+OQUT.CreateDate,
+OQUT.CntctCode,
+QUT1.unitMsr,
+OQUT.Comments
+,qut1.LineType,
+qut1.Project,
+OCPR.E_MailL as 'Contact',
+OCPR.Cellolar as 'Mobile Phone',
+ocpr.Tel1 as 'Tel1',
+OSLP.U_Name_Foreign as 'Sale Name contact',
+OSLP.Mobil as 'Mobile',
+OSLP.Email as 'Email-Sale',
+OCTG.PymntGroup,
+OCRD.Cardname,
+OCRD.CardFname,
+OCPR.name,
+QUT12.StreetB     AS 'Street / PO Box12',
+QUT12.StreetNoB   AS 'Street No.12',
+QUT12.BlockB      AS 'Block12',
+QUT12.CityB       AS 'City12',
+QUT12.ZipCodeB    AS 'Zip Code12',
+QUT12.CountyB     AS 'County12',
+QUT12.StateB      AS 'State12',
+QUT12.CountryB    AS 'Country/Region12',
+QUT1.U_SLD_Dis_Amount,
+OUGP.UgpCode
+FROM OQUT  
+INNER JOIN QUT1 ON OQUT.DocEntry = QUT1.DocEntry 
+LEFT JOIN OITM ON QUT1.ItemCode = OITM.ItemCode 
+LEFT JOIN OCRD ON OQUT.CardCode = OCRD.CardCode 
+LEFT JOIN CRD1 ON (OQUT.CardCode = CRD1.CardCode AND OQUT.PaytoCode = CRD1.Address AND CRD1.AdresType ='B') 
+LEFT JOIN OCPR ON OQUT.CntctCode = OCPR.CntctCode 
+LEFT JOIN NNM1 ON OQUT.Series = NNM1.Series 
+LEFT JOIN OCTG ON OQUT.GroupNum = OCTG.GroupNum
+LEFT JOIN OHEM ON OQUT.OwnerCode = OHEM.empID
+LEFT JOIN OSLP ON OQUT.SLPCODE = OSLP.SLPCODE 
+LEFT JOIN OPRJ ON QUT1.PROJECT = OPRJ.PRJCODE
+LEFT JOIN OITT ON QUT1.ItemCode = OITT.Code AND OITT.TreeType = 'S'
+LEFT JOIN QUT12 ON OQUT.DocEntry = QUT12.DocEntry
+LEFT JOIN OUGP ON QUT1.UomCode = OUGP.UgpCode
+LEFT JOIN [dbo].[@SLDT_SET_BRANCH] BRANCH ON OQUT.U_SLD_LVatBranch = BRANCH.Code , oadm
+WHERE OQUT.DocEntry = {?DocKey@}
+Order by 'No.' , 'Line No.'
