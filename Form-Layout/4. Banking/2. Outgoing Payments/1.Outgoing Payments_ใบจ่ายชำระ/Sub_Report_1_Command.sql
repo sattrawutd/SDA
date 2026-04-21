d89dd1f021e6c30295a95a4e077c2f7a54a6bea7
Pakorn130 <pakorn@sala-daeng.com>
2026-04-21 12:15:10 +0700

    update query and head , add project

--- Form-Layout/4. Banking/2. Outgoing Payments/1.Outgoing Payments_ใบจ่ายชำระ/Sub_Report_1_Command.sql
+++ Form-Layout/4. Banking/2. Outgoing Payments/1.Outgoing Payments_ใบจ่ายชำระ/Sub_Report_1_Command.sql
@@ -1,111 +1,45 @@
-﻿-- ============================================================
--- Report: 1.Outgoing Payments_ใบจ่ายชำระ.rpt
-Path:   4. Banking\2. Outgoing Payments\1.Outgoing Payments_ใบจ่ายชำระ.rpt
-Extracted: 2026-04-09 15:22:48
--- Source: Subreport [Report]
--- Table:  Command
--- ============================================================
+﻿SELECT 
+    T2.InvType,
+    T2.DocNum as DocNum,
+    
+    COALESCE(T3_PCH.DocDate, T3_DPO.DocDate, T3_RPC.DocDate, T3_JDT.RefDate) AS DocDate,
+    COALESCE(T3_PCH.NumAtCard, T3_DPO.NumAtCard, T3_RPC.NumAtCard, T3_JDT.Ref2) AS NumAtCard,
+    
+    Project = CASE
+        WHEN T2.InvType = 18 THEN (SELECT TOP 1 Project FROM PCH1 WHERE DocEntry = T2.DocEntry AND ISNULL(Project, '') <> '')
+        WHEN T2.InvType = 204 THEN (SELECT TOP 1 Project FROM DPO1 WHERE DocEntry = T2.DocEntry AND ISNULL(Project, '') <> '')
+        WHEN T2.InvType = 19 THEN (SELECT TOP 1 Project FROM RPC1 WHERE DocEntry = T2.DocEntry AND ISNULL(Project, '') <> '')
+        WHEN T2.InvType IN (46, 30) THEN (SELECT TOP 1 Project FROM JDT1 WHERE TransId = T2.DocTransId AND ISNULL(Project, '') <> '')
+        ELSE NULL
+    END,
 
-select 
-		T2.InvType,
-		T2.DocNum as DocNum,
-		T3.DocDate,
-		T3.NumAtCard,
-	Amount = 
-	case 
-	when T2.[AppliedFC] = 0 then T2.[SumApplied] 
-	else T2.[AppliedFC] 
-	end ,
+    Amount = CASE 
+        WHEN T2.InvType = 19 THEN 
+            CASE WHEN T2.[AppliedFC] = 0 THEN T2.[SumApplied] * -1 ELSE T2.[AppliedFC] * -1 END
+        WHEN T2.InvType IN (46, 30) THEN 
+            T2.AppliedSys
+        ELSE 
+            CASE WHEN T2.[AppliedFC] = 0 THEN T2.[SumApplied] ELSE T2.[AppliedFC] END
+    END,
 
-	AmountTax =
-	case 
-	when T2.[AppliedFC] = 0 then T2.[SumApplied] + T2.[WtAppld]
-	else T2.[AppliedFC] + T2.[WtAppldFC]
-	end ,
+    AmountTax = CASE 
+        WHEN T2.InvType = 19 THEN 
+            CASE WHEN T2.[AppliedFC] = 0 THEN (T2.[SumApplied] + T2.[WtAppld]) * -1 ELSE (T2.[AppliedFC] + T2.[WtAppldFC]) * -1 END
+        ELSE 
+            CASE WHEN T2.[AppliedFC] = 0 THEN (T2.[SumApplied] + T2.[WtAppld]) ELSE (T2.[AppliedFC] + T2.[WtAppldFC]) END
+    END,
 
-		T2.DocTransId,
-		T3.DocNum as DocNumINV,
-		T2.InvoiceId  
+    T2.DocTransId,
+    COALESCE(T3_PCH.DocNum, T3_DPO.DocNum, T3_RPC.DocNum, T3_JDT.Number) as DocNumINV,
+    T2.InvoiceId  
 
-	From VPM2 T2 
-	INNER JOIN ODPO T3 ON T2.InvType = 204 and T2.DocEntry = T3.docEntry   
+FROM VPM2 T2 
+LEFT JOIN ODPO T3_DPO ON T2.InvType = 204 AND T2.DocEntry = T3_DPO.docEntry 
+LEFT JOIN OPCH T3_PCH ON T2.InvType = 18 AND T2.DocEntry = T3_PCH.docEntry 
+LEFT JOIN ORPC T3_RPC ON T2.InvType = 19 AND T2.DocEntry = T3_RPC.docEntry 
+LEFT JOIN OJDT T3_JDT ON T2.InvType IN (46, 30) AND T2.DocTransId = T3_JDT.TransId 
+LEFT JOIN OVPM T5 ON T2.InvType IN (46, 30) AND T5.TransId = T2.DocEntry
 
-Union all
+WHERE T2.InvType IN (18, 19, 204, 30, 46)
 
-select 
-		T2.InvType,
-		T2.DocNum as DocNum,
-		T3.DocDate,
-		T3.NumAtCard,
-
-	Amount = 
-	case 
-	when T2.[AppliedFC] = 0 then T2.[SumApplied] 
-	else T2.[AppliedFC] 
-	end ,
-
-	AmountTax =
-	case 
-	when T2.[AppliedFC] = 0 then T2.[SumApplied] + T2.[WtAppld]
-	else T2.[AppliedFC] + T2.[WtAppldFC]
-	end ,
-
-		T2.DocTransId,
-		T3.DocNum as DocNumINV,
-		T2.InvoiceId   
-
-	From VPM2 T2 
-	INNER JOIN OPCH T3 ON T2.InvType = 18 and T2.DocEntry = T3.docEntry 
-
-Union all
-
-select 
-		T2.InvType,
-		T2.DocNum as DocNum,
-		T3.DocDate,
-		T3.NumAtCard,
-
-	Amount = 
-	case 
-	when T2.[AppliedFC] = 0 then T2.[SumApplied]*-1 
-	else T2.[AppliedFC]*-1 
-	end ,
-
-	AmountTax =
-	case 
-	when T2.[AppliedFC] = 0 then (T2.[SumApplied] + T2.[WtAppld])*-1
-	else (T2.[AppliedFC] + T2.[WtAppldFC])*-1
-	end ,
-
-		T2.DocTransId,
-		T3.DocNum as DocNumINV,
-		T2.InvoiceId 
-		
-	From VPM2 T2 
-	INNER JOIN ORPC T3 ON T2.InvType = 19 and T2.DocEntry = T3.docEntry 
-
-Union all
-
-select 
-		T2.InvType,
-		T2.DocNum as DocNum,
-		T3.refDate as DocDate,
-		T3.Ref2 as NumAtCard,
-		T2.AppliedSys,
-
-	AmountTax =
-	case 
-	when T2.[AppliedFC] = 0 then (T2.[SumApplied] + T2.[WtAppld])
-	else (T2.[AppliedFC] + T2.[WtAppldFC])
-	end ,
-
-		T2.DocTransId,
-		T3.Number as DocNumINV,
-		T2.InvoiceId     
-
-	From VPM2 T2 
-	INNER JOIN OJDT T3 ON T2.DocTransId = T3.TransId 
-	LEFT JOIN OVPM T5 ON T5.TransId = T2.DocEntry
-where T2.InvType in (46,30)
-
-order by T2.InvoiceId  
+ORDER BY T2.InvoiceId
